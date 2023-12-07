2ea99b1b7848e6bab6d50f52cacee985f0f228a3
BnKeTia <bernardnketia51@gmail.com>
2023-12-07 18:51:51 +0000

    added a few

--- Airtrial/console.py
+++ Airtrial/console.py
@@ -1,9 +1,30 @@
+#!/usr/bin/python3
 import cmd
 """A basic console called BnkeTia"""
 
 
 class Console(cmd.Cmd):
+    intro = "Welcome to BB console, edit your website, Kings"
     prompt = "BnB$ "
 
+    def yo_help(self, arg):
+        """Show help message"""
+        super().do_help(arg)
 
-Console().cmdloop()
+
+    def yo_quit(self, arg):
+        """Setting condition to exit the console"""
+        print("logging out...")
+        return True
+
+    def emptyline(self):
+        """Called when an empty line is entered"""
+        pass
+
+    def default(self, line):
+        """Called on an uknown command"""
+        print(f"Uknown command: {line}. Type 'help' for compartible commands")
+
+if __name__ == "__main__":
+
+    Console().cmdloop()
